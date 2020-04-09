7d2737a67dece73d46e81da6e170b267c0eabc24
Sebgatullah <sebgatullah.cuet.cse14@gmail.com>
2020-04-10 05:55:13 +0600

    Node Http Server Setup 1

--- node-http/index.js
+++ node-http/index.js
@@ -1,1 +1,18 @@
 const http = require('http');
+
+const host = "localhost";
+
+const port = 3000;
+
+const server = http.createServer((req,res)=>{
+   console.log(req.headers);
+   
+   res.statusCode = 200;
+   res.setHeader('Content-Type', 'text/html');
+   res.end("<html><body><h1>Hello!!</h1></body></html>");
+});
+
+server.listen(port, host, ()=>{
+
+    console.log(`Server running at http://${host}:${port}`);
+});
