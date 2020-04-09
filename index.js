dd088a303983ba161c778c44591c7444039408d5
Sebgatullah <sebgatullah.cuet.cse14@gmail.com>
2020-04-10 02:40:57 +0600

    Node Callbacks and Error Handling

--- index.js
+++ index.js
@@ -11,9 +11,22 @@ function solveRect(l,b){
 
 }
 function solveRectMod(l,b){
-    console.log("Area= ", rectmod.area(l,b));
-    console.log("perimeter= ", rectmod.perimeter(l,b))
+    console.log("Solving for rectangle with l = "
+                + l + " and b = " + b);
+        rectmod(l,b, (err,rectangle) => {
+        if (err) {
+	        console.log("ERROR: ", err.message);
+	    }
+        else {
+            console.log("The area of the rectangle of dimensions l = "
+                + l + " and b = " + b + " is " + rectangle.area());
+            console.log("The perimeter of the rectangle of dimensions l = "
+                + l + " and b = " + b + " is " + rectangle.perimeter());
+        }
+    });
+    console.log("This statement after the call to rect()");
 
 }
 solveRect(2,3);
 solveRectMod(2,3);
+solveRectMod(-2,0);
