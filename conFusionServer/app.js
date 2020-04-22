bb9542d2c1d6b003342aa8e561c226cfa497015e
Sebgatullah <sebgatullah.cuet.cse14@gmail.com>
2020-04-22 20:38:38 +0600

    Express Session 02

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -42,37 +42,17 @@ app.use(session({
   store: new FileStorage()
   
 }));
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 function auth(req,res,next){
    if(!req.session.user){
-    var authHeader = req.headers.authorization;
-
-   if(!authHeader){
-     var err = new Error('You are not authenticated');
-     res.setHeader('WWW-Authenticate', 'Basic');
-     err.status =401;
-     return next(err);
-   }
-
-   var auth = new Buffer.from(authHeader.split(' ')[1],'base64').toString().split(':');
-   
-   var username = auth[0];
-   var password = auth[1];
-
-   if(username === 'admin' && password ==='password'){
-     //res.cookie('user', 'admin', {signed:true});
-     req.session.user = 'admin';
-     next();
-   }
-   else{
-    var err = new Error('You are not authenticated');
-    res.setHeader('WWW-Authenticate', 'Basic');
-    err.status =401;
-    return next(err);
-   }
+    var err = new Error('You are not authenticated!');
+      err.status = 403;
+      return next(err);
   
    }
    else{
-     if(req.session.user === 'admin'){
+     if(req.session.user === 'authentiated'){
       console.log('req.session: ',req.session);
        next();
      }
@@ -88,8 +68,7 @@ function auth(req,res,next){
 app.use(auth);
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
 app.use('/dishes', dishRouter);
 
 app.use('/promotions', promoRouter);
