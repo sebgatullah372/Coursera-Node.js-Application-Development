be8cc2bf42c7dd1217c1791f911a2d57256c145b
Sebgatullah <sebgatullah.cuet.cse14@gmail.com>
2020-04-28 03:19:40 +0600

    Facebook Passport

--- conFusionServer/config.js
+++ conFusionServer/config.js
@@ -1,4 +1,8 @@
 module.exports = {
-    secretKey: '1234-3234-32345-9845',
-    mongourl: 'mongodb://localhost:27017/conFusion' 
+    'secretKey': '1234-3234-32345-9845',
+    'mongourl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: 'Your Client Id',
+        clientSecret: 'Your Secret key'
+    } 
 }
